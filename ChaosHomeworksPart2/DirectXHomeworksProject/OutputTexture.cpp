ba3036e00b63bbf41e664ab4b2efb1b1e76ed9d1
FlyMilk <j.yontcheff@gmail.com>
2025-12-26 01:07:04 +0200

    Fixed OutputTexture.cpp and ray generation is working.

--- ChaosHomeworksPart2/DirectXHomeworksProject/OutputTexture.cpp
+++ ChaosHomeworksPart2/DirectXHomeworksProject/OutputTexture.cpp
@@ -2,21 +2,32 @@
 
 OutputTexture::OutputTexture(ID3D12Device* device, const UINT& width, const UINT& height)
 {
-	resourceDesc = {};
-	resourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
-	resourceDesc.Width = width;
-	resourceDesc.Height = height;
-	resourceDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-	resourceDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS;
+    // Fill resource description
+    resourceDesc = {};
+    resourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+    resourceDesc.Alignment = 0;
+    resourceDesc.Width = width;
+    resourceDesc.Height = height;
+    resourceDesc.DepthOrArraySize = 1;
+    resourceDesc.MipLevels = 1;
+    resourceDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    resourceDesc.SampleDesc.Count = 1;
+    resourceDesc.SampleDesc.Quality = 0;
+    resourceDesc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
+    resourceDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS;
 
-	heapProperties = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT);
+    // Use correct heap properties
+    heapProperties = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT);
 
-	HRESULT hr = device->CreateCommittedResource(&heapProperties,
-		D3D12_HEAP_FLAG_ALLOW_ALL_BUFFERS_AND_TEXTURES,
-		&resourceDesc,
-		D3D12_RESOURCE_STATE_GENERIC_READ,
-		nullptr,
-		IID_PPV_ARGS(&d3d12Resource));
-	assert(SUCCEEDED(hr));
+    // FIXED: Use correct initial state for UAV texture
+    HRESULT hr = device->CreateCommittedResource(
+        &heapProperties,
+        D3D12_HEAP_FLAG_NONE,  // Changed from ALLOW_ALL_BUFFERS_AND_TEXTURES
+        &resourceDesc,
+        D3D12_RESOURCE_STATE_UNORDERED_ACCESS,  // Changed from GENERIC_READ
+        nullptr,
+        IID_PPV_ARGS(&d3d12Resource)
+    );
 
+    assert(SUCCEEDED(hr));
 }
